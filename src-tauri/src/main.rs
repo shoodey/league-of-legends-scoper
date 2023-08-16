56632b73747a284b6e4dbea4712446ef3400f705
Ali EL AMRI <contact@alielamri.com>
2023-08-16 13:12:13 +0300

    get client app port and auth token

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,14 +1,78 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use std::process::Command;
+use std::str;
+use regex::Regex;
+
 #[tauri::command]
-fn greet(name: &str) -> String {
-   format!("Hello, {}!", name)
+fn get_client_app_port() -> String {
+    let shell_function_name = "ps -A | grep \"LeagueClientUx\"";
+
+    let output = Command::new("sh")
+        .arg("-c")
+        .arg(format!("{}", shell_function_name))
+        .output()
+        .expect("Failed to execute command");
+
+    // Convert the output bytes to a String
+    let output_string = str
+        ::from_utf8(&output.stdout)
+        .expect("Failed to convert output to string")
+        .trim()
+        .to_string();
+
+    // Define the regular expression pattern
+    let pattern = r"--app-port=[0-9]*";
+
+    // Create a regular expression object
+    let re = Regex::new(pattern).expect("Failed to create regex");
+
+    // Find and collect the matched substrings into a Vec<String>
+    let mut matches: Vec<String> = Vec::new();
+    for match_ in re.find_iter(&output_string) {
+        matches.push(match_.as_str().to_string());
+    }
+
+    format!("{}", matches[0].replace("--app-port=", ""))
+}
+
+#[tauri::command]
+fn get_client_auth_token() -> String {
+  let shell_function_name = "ps -A | grep \"LeagueClientUx\"";
+
+  let output = Command::new("sh")
+      .arg("-c")
+      .arg(format!("{}", shell_function_name))
+      .output()
+      .expect("Failed to execute command");
+
+  // Convert the output bytes to a String
+  let output_string = str
+      ::from_utf8(&output.stdout)
+      .expect("Failed to convert output to string")
+      .trim()
+      .to_string();
+
+  // Define the regular expression pattern
+  let pattern = r"--remoting-auth-token=[0-9a-zA-Z\-]*";
+
+  // Create a regular expression object
+  let re = Regex::new(pattern).expect("Failed to create regex");
+
+  // Find and collect the matched substrings into a Vec<String>
+  let mut matches: Vec<String> = Vec::new();
+  for match_ in re.find_iter(&output_string) {
+      matches.push(match_.as_str().to_string());
+  }
+
+  format!("{}",matches[0].replace("--remoting-auth-token=", ""))
 }
 
 fn main() {
-  tauri::Builder::default()
-    .invoke_handler(tauri::generate_handler![greet])
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    tauri::Builder
+        ::default()
+        .invoke_handler(tauri::generate_handler![get_client_app_port, get_client_auth_token])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
--- src/components/client-info.tsx
+++ src/components/client-info.tsx
@@ -4,14 +4,22 @@ import { useEffect, useState } from "react";
 import { invoke } from "@tauri-apps/api/tauri";
 
 export default function ClientInfo() {
-  const [message, setMessage] = useState<string>("");
+  const [appPort, setAppPort] = useState<string>("");
+  const [authToken, setAuthToken] = useState<string>("");
 
   useEffect(() => {
-    invoke<string>("greet", { name: "Johnny" })
-      .then(setMessage)
+    invoke<string>("get_client_app_port").then(setAppPort).catch(console.error);
+    invoke<string>("get_client_auth_token")
+      .then(setAuthToken)
       .catch(console.error);
   }, []);
 
   // Necessary because we will have to use Greet as a component later.
-  return <>{message}</>;
+  return (
+    <pre>
+      {appPort} -{" "}
+      {Buffer.from(`riot:${authToken}`, "binary").toString("base64")} -{" "}
+      {authToken}
+    </pre>
+  );
 }
